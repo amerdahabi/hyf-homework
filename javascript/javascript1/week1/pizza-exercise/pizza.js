717435267431ee14fe5949627c554c978b2cfd5a
amerdahabi <aamerdahabi@gmail.com>
2019-11-04 23:24:33 +0100

    step2

---
+++ javascript/javascript1/week1/pizza-exercise/pizza.js
@@ -0,0 +1,7 @@
+console.log("I LOVE PIZZA")
+
+const pizza1='four season';
+const price=60;
+const familysizeprice= price * 2;
+
+    console.log("New pizza order "+pizza1+" Price of the pizza is "+price)
