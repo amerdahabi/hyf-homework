0357d9cefbe119d0d602313d317c172217d65b34
amerdahabi <aamerdahabi@gmail.com>
2020-05-15 14:43:55 +0200

    HW deleted after pushing it to the master branch by mistake

--- nodejs/week1/src/backend/routes/reservation.js
+++
@@ -1,8 +0,0 @@
-const reservationArray = require("../data/reservations.json");
-
-const response = function(request, response) {
-  const random = Math.floor(Math.random() * reservationArray.length);
-  response.json(reservationArray[random]);
-};
-
-module.exports = response;
